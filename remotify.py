282e0f72b7ac7396c74933dc5ecd11eeb9829304
gregoire <gregoire_brebner@hotmail.com>
2024-09-01 12:29:42 +0200

    Make client check for a response

--- remotify.py
+++ remotify.py
@@ -2,6 +2,7 @@ import remotifyCommon as common
 
 import logging
 import argparse
+import asyncio
 from websockets.sync.client import connect
 
 logger = logging.getLogger(__name__)
@@ -20,16 +21,26 @@ Start an interactive session to send media control commands to a remotifier serv
     parser.add_argument("host", help="IP or name of the machine to control")
     return parser.parse_args()
 
-def main(): 
+async def main(): 
     logging.basicConfig(filename='remotify.log', level=logging.DEBUG)
     args = initArgParser()
-    with connect(f"ws://{args.host}:{common.DEFAULT_PORT}") as websocket:
+    reconnect = connectToServer(args.host)
+    while reconnect:
+        connectToServer(args.host)
+            
+            
+async def connectToServer(host:str) -> bool:
+    async with connect(f"ws://{host}:{common.DEFAULT_PORT}") as websocket:
         while True:
             try:
                 websocket.send(input(""))
+                websocket.recv()
             except KeyboardInterrupt:
                 logger.info("Interrupt received, terminating session.")
-                break
+                return False
+            except Exception as e:
+                logger.error("Exception during connection", e)
+                return True
             
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
